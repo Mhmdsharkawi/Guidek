952cca9e5cf926ee9eede80a811c0d09e41a1738
Mhmdsharkawi <mohammadalsharkawi2@gmail.com>
2024-07-28 20:50:26 +0300

    Update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 const String GEMINI_API_KEY = "AIzaSyDjIYwHiR55zgCiLCLJHb-UIBH-gPlrQjE";//place your api key
-=======
-const String GEMINI_API_KEY = "AIzaSyDjIYwHiR55zgCiLCLJHb-UIBH-gPlrQjE";//place your api key
->>>>>>> fd64b8b2e850f6abff25c8a4a595333cf9fa01a9
+
+
