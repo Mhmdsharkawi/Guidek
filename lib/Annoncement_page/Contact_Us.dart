9eeac80348b4d2257dce9ca46d8ac73a70dc0e1a
Mhmdsharkawi <mohammadalsharkawi2@gmail.com>
2024-08-06 13:47:07 +0300

    Update Contact_Us.dart

--- lib/Annoncement_page/Contact_Us.dart
+++ lib/Annoncement_page/Contact_Us.dart
@@ -10,18 +10,18 @@ class ContactUsPage extends StatelessWidget {
           'Contact Us',
           style: TextStyle(
             fontFamily: 'Acumin Variable Concept',
-            fontSize: 30,
-            fontWeight: FontWeight.normal,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
             color: Colors.white,
           ),
         ),
       ),
       body: Container(
         width: double.infinity,
-        height: double.infinity,  // Ensures the container covers the full height
+        height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/contact-us.jpg'), // استبدل بمسار الصورة الفعلي
+            image: AssetImage('assets/contact-us.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
               Colors.black.withOpacity(0.5),
@@ -29,57 +29,67 @@ class ContactUsPage extends StatelessWidget {
             ),
           ),
         ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Developers',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 12,
+              color: Color(0xFFFDCD90),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Developers',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    _buildInfoSection(
+                      Icons.email,
+                      'E-mail',
+                      [
+                        'mohammadalsharkawi2@gmail.com',
+                        'obaidullahkh204@gmail.com',
+                        'fadfdsa@bgfs.dsd',
+                        'nsourzaid1@gmail.com',
+                      ],
+                      Color(0xFFfdcd90), 
+                    ),
+                    SizedBox(height: 20),
+                    _buildInfoSection(
+                      Icons.phone,
+                      'Mobile',
+                      [
+                        'Mohammad Alsharkawi: 0795508192',
+                        'Obaidullah Khuraisat: 0771001023',
+                        'Salah El-Din Anouqa: 0799713509',
+                        'Zaid Al_Nsour: 0782494403',
+                      ],
+                      Color(0xff3a8c31),  
+                    ),
+                    SizedBox(height: 20),
+                    _buildInfoSection(
+                      Icons.code,
+                      'Github',
+                      [
+                        'mhmdsharkawi',
+                        'obaidullahkh2003',
+                        'SalahAnnuka',
+                        'zaidNsour',
+                      ],
+                      Color(0xFF9C27B0),  
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              _buildInfoSection(
-                Icons.email,
-                'E-mail',
-                [
-                  'mohammadalsharkawi2@gmail.com',
-                  'obaidullahkh204@gmail.com',
-                  'fadfdsa@bgfs.dsd',
-                  'dfvmjkls@htrs.gv',
-                ],
-                Color(0xFFfdcd90), // Yellow color
-              ),
-              SizedBox(height: 20),
-              _buildInfoSection(
-                Icons.phone,
-                'Mobile',
-                [
-                  'Mohammad Alsharkawi: 0795508192',
-                  'Obaidullah Khuraisat: 0771001023',
-                  'Salah El-Din Anouqa: 0799713509',
-                  'Zaid Al_Nsour: 0782494403',
-                ],
-                Color(0xff3a8c31), // Green color
-              ),
-              SizedBox(height: 20),
-              _buildInfoSection(
-                Icons.code,
-                'Github',
-                [
-                  'mhmdsharkawi',
-                  'obaidullahkh2003',
-                  'SalahAnnuka',
-                  'UsrFourz',
-                ],
-                Color(0xFF9C27B0), // Purple color
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
