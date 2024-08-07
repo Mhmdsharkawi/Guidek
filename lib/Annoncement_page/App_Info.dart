1c096bdc29e12a6491ba5dd1e402e9099b17970b
Mhmdsharkawi <mohammadalsharkawi2@gmail.com>
2024-08-07 15:43:24 +0300

    Update App_Info.dart

--- lib/Annoncement_page/App_Info.dart
+++ lib/Annoncement_page/App_Info.dart
@@ -32,128 +32,148 @@ class AppInfoPage extends StatelessWidget {
       ),
       body: Container(
         color: Color(0xFFDCF5DC),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 12,
-                  color: Color(0xFFfdcd90),
-                ),
-                SizedBox(height: 60),
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(20),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 12,
+                color: Color(0xFFfdcd90),
+              ),
+            ),
+            SafeArea(
+              child: Column(
+                children: [
+                  Container(
+                    height: 12,
+                    color: Color(0xFFfdcd90),
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset(
-                      'assets/guidek[1].png',
-                      fit: BoxFit.cover,
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 60),
+                          Container(
+                            width: 200,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                'assets/guidek[1].png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'v 1.0',
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Acumin Variable Concept',
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                            margin: EdgeInsets.symmetric(horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF2E7D32),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 10,
+                                  offset: Offset(0, 5),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Created By:',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Front-end/UI Design:',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 21,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 7),
+                                Text(
+                                  '- Mohammad Al_Sharkawi\n- Obaidullah Khuraisat\n- Salah El-Din Anouqa ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Back-end/Database:',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 21,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 7),
+                                Text(
+                                  '- Zaid Al_Nsour\n- Obaidullah Khuraisat',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Project Mentor:',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                                SizedBox(height: 7),
+                                Text(
+                                  ' - Adnan Al-Rabea',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Acumin Variable Concept',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'v 1.0',
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Acumin Variable Concept',
-                  ),
-                ),
-                SizedBox(height: 30),
-                Container(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF2E7D32),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 10,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Created By:',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Front-end/UI Design:',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 21,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 7),
-                      Text(
-                        '- Mohammad Al_Sharkawi\n- Obaidullah Khuraisat\n- Salah El-Din Anouqa ',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Back-end/Database:',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 21,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 7),
-                      Text(
-                        '- Zaid Al_Nsour\n- Obaidullah Khuraisat',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Project Mentor:',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                      SizedBox(height: 7),
-                      Text(
-                        ' - Adnan Al-Rabea',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'Acumin Variable Concept',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
