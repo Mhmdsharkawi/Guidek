0de0932ff2dd299a8df86e76a999fc57e0948622
obaidullahkh2003 <obaidullahkh204@gmail.com>
2024-08-10 15:37:56 +0300

    api linked

--- lib/Signup&Login/login.dart
+++ lib/Signup&Login/login.dart
@@ -41,13 +41,13 @@ class _LoginPageState extends State<LoginPage> {
       return 'Please enter your password';
     } else if (value.length < 8) {
       return 'Password must be at least 8 characters';
-    } /*else if (!RegExp(r'[A-Z]').hasMatch(value)) {
+    } else if (!RegExp(r'[A-Z]').hasMatch(value)) {
       return 'Password must contain at least one uppercase letter';
-    }*/ else if (!RegExp(r'[0-9]').hasMatch(value)) {
+    } else if (!RegExp(r'[0-9]').hasMatch(value)) {
       return 'Password must contain at least one number';
-    } //else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-    //return 'Password must contain at least one special character';
-    //}
+    } else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
+    return 'Password must contain at least one special character';
+    }
     return null;
   }
 
@@ -57,8 +57,7 @@ class _LoginPageState extends State<LoginPage> {
         _isLoading = true;
       });
 
-      final url = 'https://guidekproject.onrender.com/auth/login';  // Update to match your local setup
-
+      final url = 'https://guidekproject.onrender.com/auth/login';
       try {
         final response = await http.post(
           Uri.parse(url),
