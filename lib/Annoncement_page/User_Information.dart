1bab0af719605cb04c200a4e59b8ba5b40331cfd
obaidullahkh2003 <obaidullahkh204@gmail.com>
2024-08-18 18:12:05 +0300

    user profile update working 100%

--- lib/Annoncement_page/User_Information.dart
+++ lib/Annoncement_page/User_Information.dart
@@ -6,6 +6,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 class UserProfilePage extends StatefulWidget {
   @override
   _UserProfilePageState createState() => _UserProfilePageState();
@@ -19,7 +20,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
   late String _major;
   late bool _isLoading;
   late String _accessToken;
-  ImageProvider _profileImage = AssetImage('assets/default_image.jpg'); // Provide a default value
+  ImageProvider _profileImage = AssetImage('assets/default_image.jpg'); // Default image
 
   final _formKey = GlobalKey<FormState>();
   final _picker = ImagePicker();
@@ -29,7 +30,6 @@ class _UserProfilePageState extends State<UserProfilePage> {
   void initState() {
     super.initState();
     _loadUserData();
-    _loadProfileImage();
   }
 
   Future<void> _loadUserData() async {
@@ -39,7 +39,6 @@ class _UserProfilePageState extends State<UserProfilePage> {
 
     final prefs = await SharedPreferences.getInstance();
     _accessToken = prefs.getString('accessToken') ?? '';
-    final filename = prefs.getString('userImgUrl') ?? '';
 
     try {
       // Fetch user info
@@ -59,9 +58,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
           _email = data['email'] ?? '';
           _phone = data['phone'] ?? '';
           _major = data['major_name'] ?? ''; // Add major field
-          _profileImage = filename.isNotEmpty
-              ? NetworkImage('https://guidekproject.onrender.com/users/get_image/$filename')
-              : AssetImage('assets/default_image.jpg');
+
+          // Update profile image URL
+          _updateProfileImage(data['userImgUrl'] ?? '');
           _isLoading = false;
         });
       } else {
@@ -75,38 +74,23 @@ class _UserProfilePageState extends State<UserProfilePage> {
     }
   }
 
-  Future<void> _loadProfileImage() async {
-    final prefs = await SharedPreferences.getInstance();
-    final filename = prefs.getString('userImgUrl');
-    final accessToken = prefs.getString('accessToken');
-
-    String imageUrl;
-    if (filename != null && filename.isNotEmpty) {
-      imageUrl = 'https://guidekproject.onrender.com/users/get_image/$filename';
-    } else {
-      imageUrl = 'https://guidekproject.onrender.com/users/get_image/default_image.jpg';
-    }
+  Future<void> _updateProfileImage(String filename) async {
+    final imageUrl = filename.isNotEmpty
+        ? 'https://guidekproject.onrender.com/users/get_image/$filename'
+        : 'https://guidekproject.onrender.com/users/get_image/default_image.jpg';
 
     try {
       final response = await http.get(
         Uri.parse(imageUrl),
-        headers: <String, String>{
-          'Authorization': 'Bearer $accessToken',
+        headers: {
+          'Authorization': 'Bearer $_accessToken',
         },
       );
 
       if (response.statusCode == 200) {
-        final contentType = response.headers['content-type'];
-        if (contentType != null && contentType.startsWith('image/')) {
-          setState(() {
-            _profileImage = MemoryImage(response.bodyBytes);
-          });
-        } else {
-          print('Response is not an image, content-type: $contentType');
-          setState(() {
-            _profileImage = AssetImage('assets/default_image.jpg');
-          });
-        }
+        setState(() {
+          _profileImage = MemoryImage(response.bodyBytes);
+        });
       } else {
         print('Failed to load profile image, status code: ${response.statusCode}');
         setState(() {
@@ -145,6 +129,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
       );
       request.headers['Authorization'] = 'Bearer $accessToken';
 
+      // Extract the file name from the image path
+      final fileName = _profileImageFile!.path.split('/').last;
+
       // Add the image file
       request.files.add(await http.MultipartFile.fromPath(
         'image',
@@ -152,10 +139,33 @@ class _UserProfilePageState extends State<UserProfilePage> {
       ));
 
       final response = await request.send();
+      final responseBody = await response.stream.bytesToString();
+
       if (response.statusCode == 200) {
-        final responseData = await response.stream.bytesToString();
-        final data = jsonDecode(responseData);
-        print('Image uploaded successfully: ${data['message']}');
+        print('Response status: ${response.statusCode}');
+        print('Response body: $responseBody');
+
+        // Fetch the updated user info to get the correct image name
+        final userInfoResponse = await http.get(
+          Uri.parse('https://guidekproject.onrender.com/users/current_user_info'),
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
+          },
+        );
+
+        if (userInfoResponse.statusCode == 200) {
+          final userInfoData = jsonDecode(userInfoResponse.body);
+          final updatedImageName = userInfoData['img_url'] ?? '';
+
+          // Save the updated image name to SharedPreferences
+          await prefs.setString('userImgUrl', updatedImageName);
+          print('Updated image name saved: $updatedImageName');
+        } else {
+          print('Failed to fetch updated user info');
+        }
+
+        print('Image uploaded successfully: ${jsonDecode(responseBody)['message']}');
       } else {
         print('Failed to upload image: ${response.statusCode}');
       }
@@ -164,6 +174,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
     }
   }
 
+
+
+
   Future<void> _submitForm() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save(); // Save form state
@@ -171,38 +184,71 @@ class _UserProfilePageState extends State<UserProfilePage> {
       final prefs = await SharedPreferences.getInstance();
       final accessToken = prefs.getString('accessToken') ?? '';
 
-      // Prepare the JSON data
-      final data = {
+      // Create a multipart request
+      var request = http.MultipartRequest(
+        'PUT',
+        Uri.parse('https://guidekproject.onrender.com/users/update_user_info'),
+      );
+
+      request.headers['Authorization'] = 'Bearer $accessToken';
+
+      // Create JSON data string
+      var jsonData = jsonEncode({
         'fullname': _fullname,
         'number': _studentId,
         'phone': _phone,
-        // 'major_name': _majorName, // Major is read-only
-      };
+        'major_name': _major,
+      });
+
+      // Add json_data field
+      request.fields['json_data'] = jsonData;
+
+      // Add image file if available
+      if (_profileImageFile != null) {
+        var file = await http.MultipartFile.fromPath(
+          'image',
+          _profileImageFile!.path,
+        );
+        request.files.add(file);
+      }
 
       try {
-        final response = await http.put(
-          Uri.parse('https://guidekproject.onrender.com/users/update_user_info'),
-          headers: {
-            'Authorization': 'Bearer $accessToken',
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode(data),
-        );
+        var response = await request.send();
+        final responseBody = await response.stream.bytesToString();
+        print('Response status: ${response.statusCode}');
+        print('Response body: $responseBody');
 
         if (response.statusCode == 200) {
-          final responseData = jsonDecode(response.body);
-          print('Form submitted successfully: ${responseData['message']}');
-          // Optionally navigate to another page or show a success message
+          // Fetch the updated user info
+          final userInfoResponse = await http.get(
+            Uri.parse('https://guidekproject.onrender.com/users/current_user_info'),
+            headers: {
+              'Authorization': 'Bearer $accessToken',
+              'Content-Type': 'application/json',
+            },
+          );
+
+          if (userInfoResponse.statusCode == 200) {
+            final userInfoData = jsonDecode(userInfoResponse.body);
+            final updatedImageName = userInfoData['img_url'] ?? '';
+
+            // Save the updated image name to SharedPreferences
+            await prefs.setString('userImgUrl', updatedImageName);
+            print('Updated image name saved: $updatedImageName');
+          } else {
+            print('Failed to fetch updated user info');
+          }
+
+          print('Form submitted successfully: ${jsonDecode(responseBody)['message']}');
         } else {
           print('Failed to submit form: ${response.statusCode}');
-          // Handle the error response
         }
       } catch (e) {
         print('Error submitting form: $e');
-        // Handle the exception
       }
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -317,7 +363,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
                     SizedBox(height: 16),
                     ElevatedButton(
                       onPressed: _submitForm,
-                      child: Text('Save Changes',style: TextStyle(color: Color(0xFFfdcd90)),),
+                      child: Text('Save Changes', style: TextStyle(color: Color(0xFFfdcd90))),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: appBarColor,
                       ),
