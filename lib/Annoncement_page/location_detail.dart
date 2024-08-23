63e4c5372f5e8a38dbcfb1ca21cdd98178f2c90a
Mhmdsharkawi <mohammadalsharkawi2@gmail.com>
2024-08-23 23:43:19 +0300

    Finish all feature in the app (Design,languges) still remain one page for langs…

--- lib/Annoncement_page/location_detail.dart
+++ lib/Annoncement_page/location_detail.dart
@@ -46,7 +46,8 @@ class _LocationDetailScreenState extends State<LocationDetailScreen> {
         return json.decode(response.body) as Map<String, dynamic>;
       } else {
         final errorData = json.decode(response.body);
-        final errorMessage = errorData['message'] ?? 'Failed to load room details';
+        final errorMessage = errorData['message'] ??
+            'Failed to load room details';
         throw Exception(errorMessage);
       }
     } catch (e) {
@@ -63,80 +64,113 @@ class _LocationDetailScreenState extends State<LocationDetailScreen> {
         title: Text(
           widget.roomName,
           textAlign: TextAlign.right,
+          style: TextStyle(
+            fontFamily: 'Acumin Variable Concept',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: roomDetails,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('No data found'));
-          }
-
-          final data = snapshot.data!;
-          final location = data['location'];
-          final direction = data['direction'];
-
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                          padding: EdgeInsets.zero,
-                        ),
-                        onPressed: () async {
-                          if (await canLaunch(location)) {
-                            await launch(location);
-                          } else {
-                            throw 'Could not open the map.';
-                          }
-                        },
-                        child: Image.asset(
-                          'assets/location.png',
-                          width: 300,
-                          height: 300,
-                        ),
-                      ),
-                      const SizedBox(height: 8.0), // Space between the image and text
-                      Text(
-                        'انقر على الصورة للحصول على الموقع',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black, // You can adjust the color if needed
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-                // Description area
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Directionality(
-                    textDirection: TextDirection.rtl,
-                    child: Text(
-                      direction,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/last_background.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
-          );
-        },
+          ),
+          FutureBuilder<Map<String, dynamic>>(
+            future: roomDetails,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData) {
+                return Center(child: Text('No data found'));
+              }
+
+              final data = snapshot.data!;
+              final location = data['location'];
+              final direction = data['direction'];
+
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              elevation: 0,
+                              padding: EdgeInsets.zero,
+                            ),
+                            onPressed: () async {
+                              if (await canLaunch(location)) {
+                                await launch(location);
+                              } else {
+                                throw 'Could not open the map.';
+                              }
+                            },
+                            child: Image.asset(
+                              'assets/location.png',
+                              width: 300,
+                              height: 300,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          // Space between the image and text
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.8),  
+                              borderRadius: BorderRadius.circular(12),  
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 8,
+                                  offset: Offset(0, 4), 
+                                ),
+                              ],
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                            child: Text(
+                              'انقر على الصورة للحصول على الموقع',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Description area
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Directionality(
+                        textDirection: TextDirection.rtl,
+                        child: Text(
+                          direction,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
